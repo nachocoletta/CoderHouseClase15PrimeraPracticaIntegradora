d1c7ef17fc5d82ef125763ecf5b2f8fe2007cfa9
Nacho <ignacio_coletta@hotmail.com>
2023-10-30 20:31:30 -0300

    update 30/10/23

--- src/app.js
+++ src/app.js
@@ -1,19 +1,32 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
 import path from 'path';
-
+import morgan from 'morgan';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import indexRouter from './routers/index.router.js';
-import { __dirname } from './helpers/utils.js';
+// import { __dirname } from './helpers/utils.js';
 import productsApiRouter from './routers/api/products.router.js'
 // import productsViewRouter from './routers/views/products.router.js'
 import cartsApiRouter from './routers/api/carts.router.js'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Obtén la ruta del directorio 'src'
+const srcDir = dirname(__dirname);
+
+// Obtén la ruta del directorio 'utils' dentro de 'src'
+const utilsDir = path.join(srcDir, 'utils');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(morgan('dev'));
 
+const publicDir = path.join(utilsDir, '../public');
+app.use(express.static(publicDir));
 app.engine('handlebars', handlebars.engine());
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'handlebars');
--- src/db/mongodb.js
+++ src/db/mongodb.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 import 'dotenv/config';
 
 const URL_DB = process.env.DB_MONGO_ATLAS;
-// const URL_DB = 'mongodb+srv://developer:Zu9gxjCZxXIBdojT@cluster0.mttdfot.mongodb.net/'
 export const init = async () => {
     try {
         console.log("URL_DB", URL_DB)
