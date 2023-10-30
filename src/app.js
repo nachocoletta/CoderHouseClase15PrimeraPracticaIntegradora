6e89b86e9e8a29798f0252794b60355bc90e5734
Nacho <ignacio_coletta@hotmail.com>
2023-10-30 09:17:44 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import path from 'path';
+
+import indexRouter from './routers/index.router.js';
+import { __dirname } from './utils.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use('/', indexRouter);
+
+app.use((error, req, res, next) => {
+  const message = `Ah ocurrido un error desconocido 😨: ${error.message}`;
+  console.log(message);
+  res.status(500).json({ status: 'error', message });
+});
+
+export default app;
